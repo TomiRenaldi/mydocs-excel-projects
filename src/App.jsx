54a532f80783b66981d9c00ca426fea9d9dddbac
TomiRenaldi <tomirenaldi2@gmail.com>
2022-07-11 22:47:40 +0700

    :spaekles: Add improve setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline">
+    <h1 className="text-5xl font-bold">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'half-screen': '50vh'
+      }
+    }
   },
   plugins: [],
 }
